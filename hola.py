4a7fbeae4194777b487fa40f9d70a653a191d164
Joakol8 <joakox720@gmail.com>
2025-04-15 15:38:20 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,13 @@
+# nombre = "Link"
+# edad = 117
+# print ("Hola", nombre,".Tu edad es:", edad)
+
+
+# nombre = input("Ingrese un nombre: ")
+# edad = input("Ingrese su edad: ")
+# print ("Hola", nombre,".Tu edad es:", edad)
+
+n1 = int(input("Ingrese un número: "))
+n2 = int(input("Ingrese un número: "))
+
+print("La multiplicación es: ", n1*n2)

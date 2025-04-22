216c8286cd6809002e4e2c1f9eb708ab2e958aa8
joakol8 <joakox720@gmail.com>
2025-04-22 15:11:43 -0400

    segundo commiit

--- hola.py
+++ hola.py
@@ -1,13 +1,15 @@
-# nombre = "Link"
-# edad = 117
-# print ("Hola", nombre,".Tu edad es:", edad)
+# # nombre = "Link"
+# # edad = 117
+# # print ("Hola", nombre,".Tu edad es:", edad)
 
 
-# nombre = input("Ingrese un nombre: ")
-# edad = input("Ingrese su edad: ")
-# print ("Hola", nombre,".Tu edad es:", edad)
+# # nombre = input("Ingrese un nombre: ")
+# # edad = input("Ingrese su edad: ")
+# # print ("Hola", nombre,".Tu edad es:", edad)
 
-n1 = int(input("Ingrese un número: "))
-n2 = int(input("Ingrese un número: "))
+# n1 = int(input("Ingrese un número: "))
+# n2 = int(input("Ingrese un número: "))
 
-print("La multiplicación es: ", n1*n2)
+# print("La multiplicación es: ", n1*n2)
+
+for i 

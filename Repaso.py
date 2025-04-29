a6679d6f9c6cfa27489eb2ec11c6776f187dedea
Joakol8 <joakox720@gmail.com>
2025-04-29 16:23:44 -0400

    Martes 20/04 2

---
+++ Repaso.py
@@ -0,0 +1,41 @@
+#1 Pida al usuario un numero y clasifiquelo como par o impar. Muestre los pares e impares de el uno hasta ese numero.
+
+# numuser = int(input("Ingrese un número: "))
+
+# if numuser % 2 == 0:
+#     print("Su número es par")
+#     print("Numeros Pares")
+#     for i in range(0, numuser+2, 2):
+#         if numuser % 2 == 0:
+#          print(i)
+
+# else:
+#     print("Su número es impar")
+#     print("Números impares:")
+#     for i in range(1, numuser+2, 2):
+#        print(i)    
+
+#2 Pida al usuario ingresar números. al final debe mostrar cuantos números se ingresaron y mostrar la suma de todos ellos
+# Para terminar debe poner la palabra salir
+
+
+# cont = 0
+# sum = 0
+
+
+# while True:
+#    usuario = input("Ingrese un número y luego presione Enter. Si terminó escriba salir:")
+#    if usuario.isdigit():
+    
+#     usuario = int(usuario)
+#     cont = cont + 1
+#     sum = sum + usuario
+
+#    else:
+#       if usuario == "salir":
+#          print(f"Usted ingresó {cont} números y la suma total de todos ellos es: {sum}")
+#          break
+#       else:
+#          print("ERROR: Palabra inválida")
+   
+   

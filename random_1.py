546e6d4cc501f4cdbc10086beb54e520b4ace3d4
Joakol8 <joakox720@gmail.com>
2025-05-02 18:34:01 -0400

    Vie 02-05-2025

---
+++ random_1.py
@@ -0,0 +1,32 @@
+# import random, time
+
+
+# j1 = 0
+# j2 = 0
+# meta = 30
+# turno = 1
+
+# while j1<=meta and j2<=meta:
+#     if turno % 2 == 0:
+#         print("Turno de J1")
+#         time.sleep(1)
+#         dado=random.randint(1,6)
+#         j1 = j1 + dado
+#         print(f"El J1 sacó {dado}.")
+#         print(f"Avanza a hasta la casilla {j1}.\n\n\n")
+#         turno = turno + 1
+#     else:
+#         print("Turno de J2")
+#         time.sleep(1)
+#         dado=random.randint(1,6)
+#         j2 = j2 + dado
+#         print(f"El J2 sacó {dado}.")
+#         print(f"Avanza a hasta la casilla {j2}.\n\n\n")
+#         turno = turno + 1
+
+# if j1>j2:
+#     print("Ha ganado el jugador 1")
+# else:
+#     print("Ha ganado el jugador 2")    
+
+print("")

4a4131ed7de8aca39af164699e4a4ae8a2daf6d5
Joakol8 <joakox720@gmail.com>
2025-05-20 16:24:59 -0400

    mar 20-05-25

---
+++ perro.py
@@ -0,0 +1,30 @@
+
+while True:
+
+    op = int(input('''Bienvenido al Lavado de Autos.
+
+                
+    1.- Cursar pago del lavado
+    2.- Ver ventas diarias
+    3.- Salir
+                
+    Ingresa la opción a la que desees acceder: '''))
+
+    match op:
+        case 1:
+            print("Pago del lavado")
+
+
+
+        case 2:
+            print("Ventas diarias xd")
+
+
+        case 3:
+            break
+
+
+        case _:
+
+            print("Error: Opción inválida")            
+
